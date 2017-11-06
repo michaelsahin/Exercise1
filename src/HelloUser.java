618e6e64d89956e36e1eb6acf1f0413bca1bf64d
Michael Sahin <michaelsahin1@web.de>
2017-11-06 09:42:53 +0100

    Hello User is added

---
+++ src/HelloUser.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HelloUser {
+	
+	private String userName;
+	
+	public HelloUser(String name)	{
+		this.setUserName(name);
+	}
+	
+	public void setUserName(String name) {
+		this.userName = name;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello " + this.getUserName());
+		
+	}
+	
+}
